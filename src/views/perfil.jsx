39a06c2387552bd3adf5225f33f16915864dbc25
EliazarAS7 <116899294+EliazarAS7@users.noreply.github.com>
2024-04-14 22:12:33 +0100

    vista de perfil terminada

--- src/views/perfil.jsx
+++ src/views/perfil.jsx
@@ -10,16 +10,18 @@ const Perfil = () => {
   const goToPrincipal = useGoPrincipalL();
 
   return (
-    <div>
-      <h1>¿Quién eres? Elige tu perfil</h1>
-      <div>
+    <div className={perfil.principal}>
+      <h1 className={perfil.titulo}>¿Quién eres? Elige tu perfil</h1>
+      <div className={perfil.perfiles}>
         <button onClick={goToPrincipal}>Paco</button>
         <button onClick={goToPrincipal}>Marisol</button>
         <button onClick={goToPrincipal}>Aaron</button>
         <button onClick={goToPrincipal}>Eliazar</button>
         <button onClick={goToPrincipal}>Luis Alberto</button>
       </div>
-      <p>Administrar Perfiles</p>
+      <button className={perfil.botonFinal}>
+        <p>Administrar Perfiles</p>
+      </button>
     </div>
   )
 }
